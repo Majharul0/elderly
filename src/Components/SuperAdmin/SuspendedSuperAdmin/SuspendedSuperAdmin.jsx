af24adc8f6a20fcd614d47ebf9eceeea3d374469
Majharul Islam <96412280+Majharul0@users.noreply.github.com>
2023-10-29 19:24:50 +0600

    bugs 3

--- src/Components/SuperAdmin/SuspendedSuperAdmin/SuspendedSuperAdmin.jsx
+++ src/Components/SuperAdmin/SuspendedSuperAdmin/SuspendedSuperAdmin.jsx
@@ -14,7 +14,7 @@ const SuspendedSuperAdmin = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Smart Home | Supper-Admin-Suspended</title>
-        <link rel="canonical" href="http://mysite.com/example" />
+        <link rel="canonical" href="" />
       </Helmet>
       <div className='md:px-8 px-2'>
         <div className="flex items-center gap-2 text-xs mt-10">
